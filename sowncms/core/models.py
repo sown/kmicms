6558b139fa906b41090aa97cfdcdbc03c4db5ec1
Dan Trickey <git@trickey.io>
2023-11-06 12:57:33 +0000

    Add some social media settings

--- sowncms/core/models.py
+++ sowncms/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from wagtail.contrib.settings.models import (
+    BaseSiteSetting,
+    register_setting,
+)
 
-# Create your models here.
+@register_setting
+class SocialMediaSettings(BaseSiteSetting):
+
+    discord_invite = models.URLField(blank=True, null=True)
+    github_org = models.CharField(blank=True, null=True, max_length=63, verbose_name="GitHub Username / Org Name")
+    class Meta:
+        verbose_name = "Social Media Settings"
