3a98c069203849ab10d753850be2c4c576de44d6
Dan Trickey <git@trickey.io>
2023-11-09 22:02:35 +0000

    Add a sitemap

--- sowncms/sowncms/settings.py
+++ sowncms/sowncms/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.sitemaps",
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
