d6de1aa00ed3b27a83cb4cdade4b2d67b251470c
Dan Trickey <git@trickey.io>
2023-12-01 20:43:10 +0000

    Remove people app for now

--- sowncms/people/wagtail_hooks.py
+++
@@ -1,7 +0,0 @@
-from wagtail.snippets.models import register_snippet
-
-from .views import DegreeViewSetGroup, PersonViewSet
-
-# Viewsets
-register_snippet(DegreeViewSetGroup)
-register_snippet(PersonViewSet)
